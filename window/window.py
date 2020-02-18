6f86adb7ee012054aace7f2720bff5d67e0a90c0
Yuoto <es921088@gmail.com>
2020-02-19 00:26:05 +0800

    add visible windohint

--- window/window.py
+++ window/window.py
@@ -2,7 +2,7 @@ from OpenGL.GL import *
 import glfw
 
 class Window:
-    def __init__(self, windowSize, windowName, monitor=None):
+    def __init__(self, windowSize, windowName, visible=True, monitor=None):
         """
 
         :param windowSize: a tuple, size of the window (x,y)
@@ -10,12 +10,13 @@ class Window:
         :param monitor: The selected monitor to be displayed. If None, then the primary monitor is used and not in full screen mode. If other monitors specified, full screen mode is used.
         """
         self.windowSize = windowSize
+        self.visible = visible
         self.monitor = monitor
         self.window = self.__setUpWindow(self.windowSize, windowName, self.monitor)
     def __setUpWindow(self,windowSize,name, monitor):
         # -------- setting window
 
-        init_glfw()
+        self.init_glfw()
         window = glfw.create_window(windowSize[0], windowSize[1], name, monitor, None)
         if not window:
             glfw.terminate()
@@ -48,16 +49,18 @@ class Window:
         # swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         glfw.swap_buffers(self.window)
 
-def init_glfw():
-    if not glfw.init():
-        print('Failed to initialize GLFW')
-        return
+    def init_glfw(self):
+        if not glfw.init():
+            print('Failed to initialize GLFW')
+            return
 
-    # configuring glfw
-    #glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR,3)
-    #glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
-    #glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
-    #glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+        # configuring glfw
 
-    # enable MSAA with 4 sub-samples
-    glfw.window_hint(glfw.SAMPLES,4)
+        glfw.window_hint(glfw.VISIBLE, self.visible)
+        #glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR,3)
+        #glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+        #glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
+        #glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
+        # enable MSAA with 4 sub-samples
+        glfw.window_hint(glfw.SAMPLES,4)
